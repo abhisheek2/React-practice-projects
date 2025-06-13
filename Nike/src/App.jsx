e40b03845cf3818d9671f25f4b9ffb11804a1e74
Abhishek Yadav <160116694+abhisheek2@users.noreply.github.com>
2025-06-13 19:16:07 +0000

    Remove Analytics component from App

--- Nike/src/App.jsx
+++ Nike/src/App.jsx
@@ -1,7 +1,7 @@
 import { useState } from 'react'
 import Header from './components/Header'
 import Body from './components/Body'
-import { Analytics } from "@vercel/analytics/next"
+
 
 function App() {
 
@@ -10,7 +10,7 @@ function App() {
       <div className='m-0 p-0'>
       <Header />
       <Body />
-      <Analytics />
+      
       </div>
     </>
   )
